84e1c5eaca243075efed08c157e476a9314a7354
hayoonleeMe <htdobad@gmail.com>
2025-01-22 21:32:59 +0900

    Portal Setup

---
+++ Source/DedicatedServers/Private/UI/Portal/PortalHUD.cpp
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/Portal/PortalHUD.h"
+#include "UI/Portal/SignIn/SignInOverlay.h"
+#include "Blueprint/UserWidget.h"
+
+void APortalHUD::BeginPlay()
+{
+	Super::BeginPlay();
+
+	check(SignInOverlayClass);
+	if (APlayerController* OwningPlayerController = GetOwningPlayerController())
+	{
+		SignInOverlay = CreateWidget<USignInOverlay>(OwningPlayerController, SignInOverlayClass);
+		if (SignInOverlay)
+		{
+			SignInOverlay->AddToViewport();
+		}
+
+		OwningPlayerController->SetInputMode(FInputModeGameAndUI());
+		OwningPlayerController->SetShowMouseCursor(true);
+	}
+}
