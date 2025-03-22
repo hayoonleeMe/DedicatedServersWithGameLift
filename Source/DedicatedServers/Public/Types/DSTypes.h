4cc5fd762e47843fc6585c32476602928e96cdc9
hayoonleeMe <htdobad@gmail.com>
2025-03-22 23:02:52 +0900

    Countdown Timer Handle

---
+++ Source/DedicatedServers/Public/Types/DSTypes.h
@@ -0,0 +1,54 @@
+﻿#pragma once
+
+#include "DSTypes.generated.h"
+
+UENUM(BlueprintType)
+enum class ECountdownTimerState : uint8
+{
+	NotStarted UMETA(DisplayName = "Not Started"),
+	Started UMETA(DisplayName = "Started"),
+	Paused UMETA(DisplayName = "Paused"),
+	Stopped UMETA(DisplayName = "Stopped")
+};
+
+UENUM(BlueprintType)
+enum class ECountdownTimerType : uint8
+{
+	LobbyCountdown UMETA(DisplayName = "Lobby Countdown"),
+	PreMatch UMETA(DisplayName = "PreMatch Countdown"),
+	Match UMETA(DisplayName = "Match Countdown"),
+	PostMatch UMETA(DisplayName = "PostMatch Countdown"),
+	None UMETA(DisplayName = "None"),
+};
+
+USTRUCT(BlueprintType)
+struct FCountdownTimerHandle
+{
+	GENERATED_BODY()
+
+	FCountdownTimerHandle()
+		: State(ECountdownTimerState::NotStarted), Type(ECountdownTimerType::None), CountdownTime(0.f), CountdownUpdateInterval(1.f)
+	{}
+
+	UPROPERTY(BlueprintReadWrite)
+	ECountdownTimerState State;
+
+	UPROPERTY(BlueprintReadWrite)
+	ECountdownTimerType Type;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float CountdownTime;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float CountdownUpdateInterval;
+
+	FTimerHandle TimerFinishedHandle;
+	FTimerHandle TimerUpdateHandle;
+	FTimerDelegate TimerFinishedDelegate;
+	FTimerDelegate TimerUpdateDelegate;
+};
+
+inline bool operator==(const FCountdownTimerHandle& lhs, const FCountdownTimerHandle& rhs)
+{
+	return lhs.Type == rhs.Type;
+}
