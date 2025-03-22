4b4db77529ea7e7d2404c7dfe2faade700a3ad7e
hayoonleeMe <htdobad@gmail.com>
2025-03-22 23:26:27 +0900

    Calculating Single-Trip Time

---
+++ Source/DedicatedServers/Private/Player/DSPlayerController.cpp
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/DSPlayerController.h"
+
+void ADSPlayerController::ReceivedPlayer()
+{
+	Super::ReceivedPlayer();
+
+	if (GetNetMode() == NM_Standalone)
+	{
+		return;
+	}
+	
+	if (IsLocalController())
+	{
+		ServerPing(GetWorld()->GetTimeSeconds());
+	}
+}
+
+void ADSPlayerController::ServerPing_Implementation(float TimeOfRequest)
+{
+	ClientPong(TimeOfRequest);	
+}
+
+void ADSPlayerController::ClientPong_Implementation(float TimeOfRequest)
+{
+	const float RoundTripTime = GetWorld()->GetTimeSeconds() - TimeOfRequest;
+	SingleTripTime = RoundTripTime * 0.5f;
+}
---
+++ Source/DedicatedServers/Public/Player/DSPlayerController.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "DSPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEDICATEDSERVERS_API ADSPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	virtual void ReceivedPlayer() override;
+
+protected:
+	UFUNCTION(Server, Reliable)
+	void ServerPing(float TimeOfRequest);
+
+	UFUNCTION(Client, Reliable)
+	void ClientPong(float TimeOfRequest);
+
+private:
+	float SingleTripTime = 0.f;
+};
