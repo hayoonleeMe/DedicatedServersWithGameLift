f9353ce72e6cd4839b31a909612ffa583d9abc64
hayoonleeMe <htdobad@gmail.com>
2025-05-10 20:53:44 +0900

    Fast Array Serializer

---
+++ Source/DedicatedServers/Private/Lobby/LobbyPlayerInfo.cpp
@@ -0,0 +1,25 @@
+﻿
+
+#include "Lobby/LobbyPlayerInfo.h"
+
+void FLobbyPlayerInfoArray::AddPlayer(const FLobbyPlayerInfo& NewPlayerInfo)
+{
+	const int32 Index = Players.Add(NewPlayerInfo);
+	MarkItemDirty(Players[Index]);
+	Players[Index].PostReplicatedAdd(*this);
+}
+
+void FLobbyPlayerInfoArray::RemovePlayer(const FString& Username)
+{
+	for (int32 Index = 0; Index < Players.Num(); ++Index)
+	{
+		FLobbyPlayerInfo& PlayerInfo = Players[Index];
+		if (PlayerInfo.Username == Username)
+		{
+			PlayerInfo.PreReplicatedRemove(*this);
+			Players.RemoveAtSwap(Index);
+			MarkArrayDirty();
+			return;
+		}
+	}
+}
